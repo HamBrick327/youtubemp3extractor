b61659b602520375571256615020e63c4271c7fc
HamBrick327 <derpykidgamer@gmail.com>
2024-02-20 17:43:22 -0700

    actually did the thing good, added proper mp3 conversion and fixed update bugs

--- spotify.py
+++ spotify.py
@@ -3,11 +3,13 @@ import os
 from pytube import Playlist, YouTube
 from youtubesearchpython import VideosSearch
 from spotipy.oauth2 import SpotifyClientCredentials
-from moviepy.editor import VideoFileClip
+import subprocess
+import eyed3
 
-spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id={"e1ca0e54413443109ff7eb696827389c"},
-                                                           client_secret={"f12b6a6d37ed499fa9660a9716756971"}))
-playlistLink = "https://open.spotify.com/playlist/7s0hReCIRegs9Q2QrsVgC9?si=57117ff2524942b8"
+spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="e1ca0e54413443109ff7eb696827389c",
+                                                           client_secret="f12b6a6d37ed499fa9660a9716756971"))
+
+playlistLink = "https://open.spotify.com/playlist/7s0hReCIRegs9Q2QrsVgC9?si=2e91b220db3643d0"
 
 
 
@@ -16,31 +18,37 @@ def searchYoutube(query: str, output_path: str="./output"):
     
     # youtubesearchpython variables
     print("seaching video " + query)
-    search = VideosSearch(query, limit=1)
+    ## adding 'lyrics' tends to give the streaming/radio versions of the song, remove if you want
+    search = VideosSearch((query + "lyrics"), limit=1)
     
     url = search.result()['result'][0]['link']
     title = search.result()['result'][0]['title']
     print("found: ", title)
     
     
-    ## doing it the pytube way
+    ## this library donwloads a .webm file
     video = YouTube(url)
     audio = video.streams.filter(only_audio=True).first() ## download only the video's audio, but still as a mp4 file
     output = audio.download(output_path=output_path) ## change to audio.download() when above line is being used
     
     ## change the file extension, ooga booga style
     name, ext = os.path.splitext(output)
-    ext = '.mp3'
+    track, artist = query.split(" : ")
+    print("converting " + name)
     try:
-        video_file = VideoFileClip(f"{name}.mp4")
-        audio_file = video_file.audio
-        audio_file.write_audiofile(f"{name}.mp3")
-        video_file.close()
-        audio_file.close()
-        os.remove(f"{name}.mp4")
+        ## uses python built in subprocess to use ffmpeg to convert the .webp audio file with a .mp4 extension to a .mp3
+        subprocess.run(['ffmpeg', '-i', (name + '.mp4'), '-vn', '-acodec', 'libmp3lame', '-y', (name + ".mp3")])
+
     except:
         print("file already exists, skipping")
-        os.remove(f"./{name}.mp4")
+    ## remove the .mp4 file regardless of if the .mp3 file existed
+    os.remove(f"{name}.mp4")
+
+    ## update the metadata a little, so vlc and other things will know what's going on
+    audioFile = eyed3.load((name + ".mp3"))
+    audioFile.tag.title = track
+    audioFile.tag.artist = artist
+    audioFile.tag.save()
     
 def spotPlaylist(link):
     ## track URL https://open.spotify.com/playlist/6yHOBv1K6lvJHbAcr61SBB?si=30169aad79f04d6a
@@ -58,17 +66,17 @@ def greaterThan100(link):
 
     ## get the playlist ID from the playlist link
     id = link.split("/")[-1].split("?")[0] ## list[-1] prints the last item of the list
-
+    ## I learned now that you can just use the playlist link directly as the playlist_id so this is technically not needed.
 
     tracks = []
     
     offset = 0
 
-    response = spotify.playlist_tracks(playlist_id="7s0hReCIRegs9Q2QrsVgC9", offset=offset)
     # ouput.write(str(response))
 
     offset = 0
     while True:
+        ## only using this to get playlist name
         playlist = spotify.playlist(playlist_id=id)
         '''
         so what this does basically is force the spotify api to grab more than 100 tracks from the playlist by setting 
@@ -85,8 +93,7 @@ def greaterThan100(link):
         # Append the tracks to the all_tracks list
         all_tracks.extend(tracks)
 
-        # Increment the offset to retrieve the next set of tracks
-        '''increasing the offset to for the api call to grab the next 100 from the playlist'''
+        ## increasing the offset to for the api call to grab the next 100 from the playlist
         offset += len(tracks)
 
     # You can access track information using the all_tracks list
@@ -100,7 +107,7 @@ def greaterThan100(link):
     try:
         os.mkdir(playlist['name'])
     except: 
-        print("playlist file already exists, continuing on")
+        print("playlist filder already exists, continuing on")
     return output, playlist['name']
 
 print(greaterThan100(playlistLink)[1])
